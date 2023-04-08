e39899143ec84034dedda148dc1a72d7ecb72788
Fatma Şahiner <ftmsahiner01@gmail.com>
2023-04-08 15:38:57 +0300

    Sahne 2 eklendi

---
+++ OUA_GameJam_U26/Assets/Script/Main_Menu.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Main_Menu : MonoBehaviour
+{
+    public void playGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+    public void mainMenu() 
+    {
+        SceneManager.LoadScene("Main Menu");
+    }
+
+    public void quitMenu() 
+    {
+        Application.Quit();
+    }
+
+
+
+}
