d6f3d44045a2797cb3157af01e9fbb64299ea95d
Despance <despance1927@gmail.com>
2023-04-09 18:01:06 +0300

    dialogue system prefabs

--- OUA_GameJam_U26/Assets/Scripts/DialogueManager.cs
+++ OUA_GameJam_U26/Assets/Scripts/DialogueManager.cs
@@ -13,7 +13,10 @@ public class DialogueManager : MonoBehaviour
 
     public string dialogueText;
     public float textSpeed = 15f;
-    private TextMeshProUGUI textBox; 
+    private TextMeshProUGUI textBox;
+    public static int dialogueIndex = 0;
+
+   
     void Start()
     {
         textBox = dialogueObject.GetComponentInChildren<TextMeshProUGUI>();
@@ -22,7 +25,7 @@ public class DialogueManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-           
+
     }
 
     public void showDialogue()
@@ -30,21 +33,64 @@ public class DialogueManager : MonoBehaviour
         dialogueObject.DOScale(Vector3.one, 0.5f).SetEase(Ease.InOutExpo);
         showText();
     }
-    
+
     public void showText()
     {
-        string text = "";
-        DOTween.To(() => text, x => text = x,dialogueText,dialogueText.Length/textSpeed).SetEase(Ease.Linear).OnUpdate(() =>
+        if (!dialogueStarted)
         {
-            textBox.text = text;
-        });
+            dialogueStarted = true;
+            string text = "";
+            DOTween.To(() => text, x => text = x, dialogueText, dialogueText.Length / textSpeed).SetEase(Ease.Linear)
+                .OnUpdate(() => { textBox.text = text; });
+        }
+        
+        
     }
 
     public void hideDialogue()
     {
-        dialogueObject.DOScale(Vector3.zero, 0.5f).SetEase(Ease.InOutExpo);
+        dialogueObject.DOScale(Vector3.zero, 0.5f).SetEase(Ease.InOutExpo).OnComplete((() =>
+        {
+            dialogueText = dialogueStrings[++dialogueIndex];
+            dialogueStarted = false;
+        }));
+        
     }
+
     
+    private string[] dialogueStrings =
+    {
+        "Prefabler aynı sahne veya proje içinde yeniden kullanılabilir oyun nesneleri oluşturmak için geliştirilmiş bir sistemdir.",
+        "Bir scriptteki değişkenin erişim türünü public yapmak, onu inspector paneli veya diğer scriptlerden erişilebilmesini mümkün kılar.",
+        "Kullanıcı tarafından yazılan kodun bilgisayar diline çevirilme işlemine compilation adı verilir.",
+        "Console'da cümle içinde kesme işareti (') kullanabilmek için aşağıdaki ifadeyi yazabiliriz \n Console.Write(\"\\\'\");",
+        "Kodumuzun içerisinde işlenmesini istemediğimiz metinleri belirtmemizi sağlayan, hem kendimiz hem de başkaları için notlar bırakabildiğimiz yapının adı \"Yorum Satırıdır\".",
+        "namespaces, diğer scriptlerden namespace altındaki class\'ları kullanabilmek için kullanılır",
+        "Boolean true false değerleri olan mantıksal veri tipidir.",
+        "Bir fonksiyonu return edebilmek için fonksiyonun türü integer, boolean,string gibi operatörler olabilir.",
+        "Switch case yapısında default Tanımlanmayan olasılık gerçekleştiğinde olacak durumu yazmamızı sağlar",
+        "Unity hub kullanarak yeni bir 2D bir projesini sırasıyla Project>New > 2D > Create aşamları uygulayarak açabiliriz.",
+        "Layout pencere düzenini istediğimiz gibi düzenlememizi sağlar.",
+        "Unity package manager Unity'e paket eklemeyi veya varolan paketi güncellemeyi ve silebilmeyi sağlar.",
+        "2D nesnemizin fizik özelliğine sahip olması ve diğer nesnelere çarpabilmesi için nesnemize Box Collider 2D ve Rigidbody 2D component'leri eklememiz gerekiyor.",
+        "Sprite kullanarak animasyon oluşturduğumuzda nesnemizde Animator component kendiliğinden eklenir.",
+        "2D bir oyun projemizde gökyüzü, platform ve karakter gibi nesnelerin sorting layer'ları şu şekilde olmalıdır: \n 1.Gökyüzü 2.Platform 3.Karakter",
+        "Unity'de animasyonların biribirine bağlandığı,istenildiğinde koşullar eklendiği bölüme Animator:State Machine adı verilir.",
+        "Oyunda karakterimin hız değişkenini \"w\" tuşuna basarak değiştirmek istiyorsak aşağıdaki kodu yazabiliriz \n Input.GetKeyDown(Keycode.W)",
+        "Koşma animasyona geçiş yapmak için koşulum klavyede a,d ve sağ,sol tuşlarından en az birine basmış olmak ise if(Input.GetAxis(\"Horizontal\")!=0) kodunu kullanabiliriz.",
+        "Animator içerisindeki bir integer parametrenin değerini değiştirmek için animator.SetInt() kodu kullanabiliriz.",
+        "Unity'de karakterimizin fizik olaylarını Fixed Update() fonksiyonun içine yazmamız daha doğru olur.",
+        "Karakterimizin konumunu kod üzerinden değiştirmek için karakterimizin Transform component'ine erişmemiz gerekiyor",
+        "Late Update() kullanmak oyun kamerasının hareketlerini kontrol etmemizde yardımcı olur.",
+        "Update fonksiyonlarının öncelik sıralaması aşağıda verilmiştir. \n 1.FixedUpdate() 2.Update() 3.LateUpdate()",
+        "Awake ve Start fonksiyonları arasındaki temel fark Awake, oyunu çalıştırdığımızda; Start, script'i çalıştırdığımızda çalışır.",
+        "Kullanılan assetin ücretsiz bir şekilde kullanılması ve dağıtılması için assetin lisansı CC0 olmalıdır.",
+        "2D oyun için arkaplan tasarımını Tilemap kullanarak yapabiliriz.",
+        "2D oyunda objelere collider ekleme Diğer objelerle temasa geçebilmesi için önemlidir.",
+        "Machine State'te parametreler oluşturmanın amacı Oluşturulan parametrelere göre animasyon geçişlerine koşul eklemek.",
+        "Arkaplanımızın kendini tekrar edecek şekilde genişlemesini istiyorsak Raw Mode'unu  Tile yapmamız gerekir.",
+        "Unity projemizde Tile Palette'imize Window-2D-Tile Palette yolu ile ulaşabiliriz."
+    };
     
 }
 
